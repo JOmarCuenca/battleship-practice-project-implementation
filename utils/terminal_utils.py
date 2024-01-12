19344bc016eed7a4b30ad9be477d66eacb9c2c34
Omar Cuenca <jesomar.cuenca@gmail.com>
2024-01-12 14:08:40 -0600

    Update clear_screen function to work on Windows, macOS, and Linux

--- utils/terminal_utils.py
+++ utils/terminal_utils.py
@@ -1,6 +1,13 @@
-from os import system
+from os import system, name as __os_name
 
 
 def clear_screen():
     '''Clears the terminal screen'''
-    system('clear')
+
+    # For Windows
+    if __os_name == 'nt':
+        system('cls')
+
+    # For macOS and Linux
+    else:
+        system('clear')
